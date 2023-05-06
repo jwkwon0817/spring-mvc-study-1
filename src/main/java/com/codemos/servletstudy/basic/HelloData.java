20ce64964ee799a42fc1d7da51dd0cc769d8e20f
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-06 16:27:31 +0900

    Hello Data

---
+++ src/main/java/com/codemos/servletstudy/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.codemos.servletstudy.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+	private String username;
+	private int age;
+}
