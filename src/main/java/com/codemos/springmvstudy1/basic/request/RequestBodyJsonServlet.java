0b433236a89b94944f1f1cda91f2bc0525d6e60b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-08 22:54:10 +0900

    Request Body JSON Servlet

--- src/main/java/com/codemos/springmvstudy1/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/com/codemos/springmvstudy1/basic/request/RequestBodyJsonServlet.java
@@ -1,6 +1,6 @@
-package com.codemos.servletstudy.basic.request;
+package com.codemos.springmvstudy1.basic.request;
 
-import com.codemos.servletstudy.basic.HelloData;
+import com.codemos.springmvstudy1.basic.HelloData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
