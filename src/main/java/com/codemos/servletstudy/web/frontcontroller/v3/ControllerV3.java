610c75398240601add93a5b3d0c70f61e3361bef
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-08 22:49:09 +0900

    Controller V3

--- src/main/java/com/codemos/servletstudy/web/frontcontroller/v3/ControllerV3.java
+++
@@ -1,9 +0,0 @@
-package com.codemos.servletstudy.web.frontcontroller.v3;
-
-import com.codemos.servletstudy.web.frontcontroller.ModelView;
-
-import java.util.Map;
-
-public interface ControllerV3 {
-	ModelView process(Map<String, String> paramMap);
-}
