b3b0e24295cf885297a8b35751defee6bf896243
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-08 22:55:10 +0900

    Servlet Initializer

--- src/main/java/com/codemos/springmvstudy1/ServletInitializer.java
+++ src/main/java/com/codemos/springmvstudy1/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.codemos.servletstudy;
+package com.codemos.springmvstudy1;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServletStudyApplication.class);
+		return application.sources(SpringMvcStudy1Application.class);
 	}
 	
 }
